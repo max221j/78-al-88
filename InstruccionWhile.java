fe4e55a539cfafec9a9b1798ca7205d1b3786472
RODRUIGUEZ <maxr75663@gmail.com>
2026-06-01 09:20:52 -0500

    Add files via upload

---
+++ InstruccionWhile.java
@@ -0,0 +1,10 @@
+public class InstruccionWhile {
+    public static void main(String[] args) {
+        int numero = 1;
+        
+        while (numero <= 10) {
+            System.out.println("Texto " + numero);
+            numero = numero + 1; // También puedes usar: numero++;
+        }
+    } // Fin del método main
+} // Fin de la clase InstruccionWhile
